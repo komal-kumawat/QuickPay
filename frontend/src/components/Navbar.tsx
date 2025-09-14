1ba3ceb00573c69fd3ce1e6aa00f4da3d64c2b2e
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-09-14 17:11:15 +0530

    resolving errors in navbar and context

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
             WebkitBackgroundClip: "text",
             WebkitTextFillColor: "transparent",
             MozBackgroundClip: "text",
-            MozTextFillColor: "transparent",
+            // MozTextFillColor: "transparent",
           }}
         >
           QuickPay
