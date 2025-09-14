136f069b477e35d5b52866b2f4fe17d22ae0b5fd
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-09-14 17:20:00 +0530

    changed the urls

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,7 +5,10 @@ import rootRouter from "./routes/index";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:["https://quick-pay-olive-ten.vercel.app"],
+  credentials:true
+}));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://quickpay-la7i.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
